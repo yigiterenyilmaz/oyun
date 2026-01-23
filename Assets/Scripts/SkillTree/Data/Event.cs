dfa3ecb8f863ad693aae4f7bdd69946975a18ea2
yigiterenyilmaz <yigiterenyilmaz05@gmail.com>
2026-01-23 15:39:15 +0300

    unlock decision effect class is deleted.

--- Assets/Scripts/SkillTree/Data/Event.cs
+++ Assets/Scripts/SkillTree/Data/Event.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "SkillTree/Event")]
 public class Event : ScriptableObject
 {
-    public string id;
-    public string displayName;
-    public string description;
-    public Sprite icon;
+    public string id;//eventin ID'si
+    public string displayName; //ismi
+    public string description; //açıklaması
+    public Sprite icon; //iconu
     public List<EventChoice> choices;
 
-    public bool isRepeatable = true;
-    public float weight = 1f;
-    public List<Skill> requiredSkills;
-    public List<StatCondition> statConditions;
-    public int minGamePhase = 0;
-    public int maxGamePhase = 99;
+    public bool isRepeatable = true; //tekrar gelip gelemeyeceği.
+    public float weight = 1f; //ağırlığı, ne sıklıkla gelebileceğini etkiler.
+    public List<Skill> requiredSkills; //event havuzuna girebilmek için hangi skillerin açılması gerektiği
+    public List<StatCondition> statConditions; //bazı eventler stat durumuna göre de gelebilir. para seviyesine, güvenilirlik ve şüphesine göre vs.
+    public int minGamePhase = 0; //oyunun min hangi noktasında gelebileceği.
+    public int maxGamePhase = 99; //oyunun max hangi noktasında gelebileceği.
 }
